37e59a13488b949ffe2e1faea2b21557b6099746
Balakrishnan Maharajan <balakrishnan.m2@htcinc.com>
2026-02-19 18:59:16 +0530

    error 9.13

--- src/main/java/com/bala/controller/SumController.java
+++ src/main/java/com/bala/controller/SumController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 public class SumController {
+    public static int[] numbers = {1, 2, 3};
 
     @PostMapping("/sum")
     public SumResponse calculateSum(@RequestBody SumRequest request) {
